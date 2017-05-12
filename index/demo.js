88ec98e64d53d769c14aa6fcedd3e57f3c5a2b66
Peppesterest <The-Peppester@users.noreply.github.com>
2017-05-11 20:39:40 -0400

    Update demo.js

--- index/demo.js
+++ index/demo.js
@@ -43,7 +43,7 @@ $(function() {
 				$preview_cContext.font = '96px Arizonia';
 				$preview_cContext.scale(-1, 1);
 				$preview_cContext.translate(-$preview_canvas.width, 0);
-				if (petsName) $preview_cContext.fillText(petsName, $preview_canvas.width/2, $preview_canvas.height - 24);
+				if (petsName) $preview_cContext.fillText(petsName, $preview_canvas.width/2 - 12, $preview_canvas.height - 24);
 				$preview_cContext.translate($preview_canvas.width, 0);
 				$preview_cContext.scale(-1, 1);
 				$download_link.href = $preview_canvas.toDataURL('image/jpeg');
