6f3fc1a58f00dab4d40ac91fbdea6537c19ec4e8
Peppesterest <The-Peppester@users.noreply.github.com>
2017-05-13 13:26:38 -0400

    Update demo.js

--- index/demo.js
+++ index/demo.js
@@ -63,11 +63,12 @@ $(function() {
 					'This text will be put at the bottom of the image. ' + 
 					'If you wish for your pet to remain anonymous '+
 					'(which it totally fine), then leave this box empty.');
-				$preview_cContext.fillText(petsName.trim(), $preview_canvas.width/2, 12);
+				$preview_cContext.fillText(petsName.trim(), $preview_canvas.width/2, 120);
 				$preview_cContext.filter = $preview_canvas.style.filter;
 				$preview_cContext.drawImage( $preview_canvas, 0, 0 );
 				$preview_cContext.translate($preview_canvas.width, 0);
 				$preview_cContext.scale(-1, 1);
+				$preview_cContext.fillText(petsName.trim(), $preview_canvas.width/2, 120);
 				
 				$preview_canvas.toBlob(function(blob) {
 					$download_link.href = URL.createObjectURL( blob );
